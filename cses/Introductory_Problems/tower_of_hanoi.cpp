3511f6b754f3d55ba470f06e259a4d337ca23f69
huyle <lqh1411@gmail.com>
2021-01-10 21:10:52 +0700

    Update code tower of hanoi Change folder name to easy compile code

---
+++ cses/Introductory_Problems/tower_of_hanoi.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<pair<int,int>> ans;
+void solve(int n, int source=1, int dest=3) {
+    if(n==1) {
+        ans.push_back({source, dest});
+        return;
+    }
+    int middle = 1;
+    for(;middle<4; ++middle) {
+        if(middle!=source && middle!=dest)
+            break;
+    }
+    solve(n-1, source, middle);
+    ans.push_back({source, dest});
+    solve(n-1, middle, dest);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    solve(n);
+    cout << ans.size() << '\n';
+    for(auto it:ans)
+        cout << it.first << ' ' << it.second << '\n';
+
+    return 0;
+}
